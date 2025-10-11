6d2d802b557f75c5a541b184df24340385fef49e
Guilherme Saldanha <guilhermesaldanha007@gmail.com>
2025-10-10 22:56:11 -0300

    feat(core): add global prefix and standardize response/error formatting
    - - BREAKING CHANGE: base da API mudou para /api/v1 e respostas foram padronizadas.
    -

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
