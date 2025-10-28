dea4e83091a5f56427db891e121d2efe7f4ebf4e
Guilherme Saldanha <guilhermesaldanha007@gmail.com>
2025-10-28 19:48:11 -0300

    chore(core): ajustes em bootstrap (main.ts) e configuração de datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,18 +5,16 @@ import { Condominium } from './condominiums/entities/condominium.entity';
 import { Unit } from './units/entities/unit.entity';
 import { Infraction } from './infractions/entities/infraction.entity';
 import { User } from './users/entities/user.entity';
-
 const appDataSourceOptions = {
-  type: 'postgres' as const,
-  host: process.env.DB_HOST || 'localhost',
-  port: +(process.env.DB_PORT || 5432),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'audicon',
-  entities: [Condominium, Unit, Infraction, User],
-  migrations: ['src/migrations/*.ts'],
-  synchronize: false,
-  logging: false,
+    type: 'postgres' as const,
+    host: process.env.DB_HOST || 'localhost',
+    port: +(process.env.DB_PORT || 5432),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_DATABASE || 'audicon',
+    entities: [Condominium, Unit, Infraction, User],
+    migrations: ['src/migrations/*.ts'],
+    synchronize: false,
+    logging: false,
 };
-
 export const AppDataSource = new DataSource(appDataSourceOptions);
--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api/v1');
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalInterceptors(new ResponseInterceptor());
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
